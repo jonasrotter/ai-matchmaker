42461e9068c9da0c320fa72bd68d7e97ec951866
Jonas Rotter <jonasrotter@microsoft.com>
2025-06-08 11:21:51 +0200

    Semantic Search tested successfully

--- utils/test_aisearch.py
+++ utils/test_aisearch.py
@@ -5,12 +5,18 @@ import dotenv
 from openai import OpenAI
 from azure.search.documents import SearchClient
 from azure.search.documents.models import VectorizedQuery
+from azure.search.documents.models import (
+    QueryAnswerType,
+    QueryCaptionType,
+    QueryType,
+    VectorizedQuery,
+)
 
 dotenv.load_dotenv()
 
 AISEARCH_ENDPOINT = os.getenv('AZURE_AISEARCH_ENDPOINT')
 AISEARCH_KEY = os.getenv('AZURE_AISEARCH_ADMIN_KEY')
-INDEX_NAME = "products-index"
+INDEX_NAME = "prod-index"
 GPT_MODEL = "gpt-4o-mini"
 EMBEDDING_MODEL = "text-embedding-3-large"
 
@@ -23,20 +29,42 @@ def get_embedding(text):
 
 
 def main():
-    user_question = "Do you have blue pants for man?"
-    user_question_vector = get_embedding(user_question)
+    query = "Do you have blue pants for men?"
+    #qqna = "Is there Wifi in the store?"
+    query_vector = VectorizedQuery(vector=get_embedding(query), k_nearest_neighbors=3, fields="text_vector")
+    print (f"Query: {query}\n")
+
+    #results = client.search(
+    #    query,
+    #    top=2, 
+    #    vector_queries=[query_vector],
+    #    select=["productDisplayName", "subCategory", "baseColour","gender"],
+    #)
 
     results = client.search(
-        user_question,
-        top=5, 
-        vector_queries=[
-                VectorizedQuery(vector=user_question_vector, k_nearest_neighbors=3, fields="text_vector")],
+        query,
+        top=2,  
+        vector_queries=[query_vector],
+        select=["productDisplayName", "subCategory", "baseColour","gender"],
+        query_type=QueryType.SEMANTIC,
+        query_caption=QueryCaptionType.EXTRACTIVE,
+        query_answer=QueryAnswerType.EXTRACTIVE,
     )
 
-    print("Test")
+    semantic_answers = results.get_answers()
+    for answer in semantic_answers:
+        if answer.highlights:
+            print(f"Semantic Answer: {answer.highlights}")
+        else:
+            print(f"Semantic Answer: {answer.text}")
+        print(f"Semantic Answer Score: {answer.score}\n")
 
     for result in results: 
-        print(result)
+        print(f"Product Name: {result['productDisplayName']}")  
+        print(f"Category: {result['subCategory']}")  
+        print(f"Colour: {result['baseColour']}") 
+        print(f"Gender: {result['gender']}\n") 
 
 if __name__ == '__main__':
     main()
+    print("Completed successfully!")
